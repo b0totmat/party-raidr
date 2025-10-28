ae9240bf90a9d5ec65380bb7e0154368b37f18e2
Tóth Máté <t0thm4t3@gmail.com>
2025-10-28 16:35:27 +0100

    Base interface for Database Entities

---
+++ PartyRaidR/PartyRaidR.Shared/Models/IDbEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace PartyRaidR.Shared.Models
+{
+    public interface IDbEntity<TEntity> where TEntity : class, new()
+    {
+        string Id { get;set; }
+        bool HasId => Id is not null && Id != string.Empty;
+        string DbSetName => string.Concat(new TEntity().GetType(), 's');
+    }
+}
