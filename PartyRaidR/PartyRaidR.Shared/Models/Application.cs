997fe3e8f0e7c97e482129c54b9a702a11de2de3
Tóth Máté <t0thm4t3@gmail.com>
2025-11-01 13:29:22 +0100

    Reworked application statuses

--- PartyRaidR/PartyRaidR.Shared/Models/Application.cs
+++ PartyRaidR/PartyRaidR.Shared/Models/Application.cs
@@ -1,5 +1,14 @@
 ﻿namespace PartyRaidR.Shared.Models
 {
+    public enum StatusType
+    {
+        NoApplicationNeeded = 0,
+        Pending = 1,
+        Accepted = 2,
+        Rejected = 3,
+        WaitList = 4
+    }
+
     public class Application : IDbEntity<Application>
     {
         public Application()
@@ -8,22 +17,22 @@
             UserId = string.Empty;
             EventId = string.Empty;
             TimeOfApplication = DateTime.Now;
-            StatusId = string.Empty;
+            Status = StatusType.NoApplicationNeeded;
         }
 
-        public Application(Guid id, string userId, string eventId, DateTime timeOfApplication, string statusId)
+        public Application(Guid id, string userId, string eventId, DateTime timeOfApplication, StatusType status)
         {
             Id = Guid.Empty.ToString();
             UserId = userId;
             EventId = eventId;
             TimeOfApplication = timeOfApplication;
-            StatusId = statusId;
+            Status = status;
         }
 
         public string Id { get; set; }
         public string UserId { get; set; }
         public string EventId { get; set; }
         public DateTime TimeOfApplication { get; set; }
-        public string StatusId { get; set; }
+        public StatusType Status { get; set; }
     }
 }
