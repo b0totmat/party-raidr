377e9423ad3cd2fac638fd11f14775e3a1c10335
Tóth Máté <t0thm4t3@gmail.com>
2025-11-01 14:28:14 +0100

    Converter classes

---
+++ PartyRaidR/PartyRaidR.Shared/Converters/CityConverter.cs
@@ -0,0 +1,27 @@
+﻿using PartyRaidR.Shared.Dtos;
+
+namespace PartyRaidR.Shared.Converters
+{
+    public static class CityConverter
+    {
+        public static CityDto ConvertToCityDto(this CityDto city) =>
+            new CityDto()
+            {
+                Id = city.Id,
+                Name = city.Name,
+                ZipCode = city.ZipCode,
+                County = city.County,
+                Country = city.Country
+            };
+
+        public static CityDto ConvertToCity(this CityDto cityDto) =>
+            new CityDto()
+            {
+                Id = cityDto.Id,
+                Name = cityDto.Name,
+                ZipCode = cityDto.ZipCode,
+                County = cityDto.County,
+                Country = cityDto.Country
+            };
+    }
+}
