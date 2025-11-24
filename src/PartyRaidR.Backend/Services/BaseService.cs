e7e805e165c9e74aba877231e603307eaf714b16
Tóth Máté <t0thm4t3@gmail.com>
2025-11-24 15:38:33 +0100

    Generic Service class

---
+++ src/PartyRaidR.Backend/Services/BaseService.cs
@@ -0,0 +1,78 @@
+﻿using PartyRaidR.Backend.Repos.Base;
+using PartyRaidR.Shared.Assemblers;
+using PartyRaidR.Shared.Models;
+
+namespace PartyRaidR.Backend.Services
+{
+    public class BaseService<TModel, TDto>
+        where TModel : class, IDbEntity<TModel>, new()
+        where TDto: class, new()
+    {
+        protected Assembler<TModel, TDto> _assembler;
+        protected IRepositoryBase<TModel> _repo;
+
+        public BaseService(Assembler<TModel, TDto>? assembler, IRepositoryBase<TModel>? repo)
+        {
+            _assembler = assembler ?? throw new ArgumentNullException($"{nameof(assembler)} was null.");
+            _repo = repo ?? throw new ArgumentNullException($"{nameof(repo)} was null.");
+        }
+
+        public virtual async Task<TDto> GetByIdAsync(string id)
+        {
+            TModel model = await _repo.GetByIdAsync(id);
+            return _assembler.ConvertToDto(model);
+        }
+
+        public virtual async Task<IEnumerable<TDto>> GetAllAsync()
+        {
+            IEnumerable<TModel> models = await _repo.GetAllAsync();
+            return models.Select(_assembler.ConvertToDto);
+        }
+
+        public virtual async Task<TDto> AddAsync(TDto dto)
+        {
+            TModel entity = _assembler.ConvertToModel(dto);
+
+            if (entity.HasId)
+                throw new Exception($"There is already a(n) {nameof(dto)} with the given ID.");
+
+            await _repo.InsertAsync(entity);
+
+            await _repo.SaveChangesAsync();
+
+            return _assembler.ConvertToDto(entity);
+        }
+
+        public virtual async Task<TDto> UpdateAsync(TDto dto)
+        {
+            TModel model = _assembler.ConvertToModel(dto);
+
+            TModel entity = await _repo.GetByIdAsync(model.Id);
+
+            if (entity == default)
+                throw new KeyNotFoundException($"Could not found a(n) {nameof(TModel)} with the given ID.");
+
+            _repo.Update(entity);
+
+            await _repo.SaveChangesAsync();
+
+            return _assembler.ConvertToDto(entity);
+        }
+
+        public virtual async Task<TDto> DeleteAsync(TDto dto)
+        {
+            TModel model = _assembler.ConvertToModel(dto);
+
+            TModel entity = await _repo.GetByIdAsync(model.Id);
+
+            if (entity == default)
+                throw new KeyNotFoundException($"Could not found a(n) {nameof(TModel)} with the given ID.");
+
+            _repo.Delete(entity);
+
+            await _repo.SaveChangesAsync();
+
+            return _assembler.ConvertToDto(entity);
+        }
+    }
+}
