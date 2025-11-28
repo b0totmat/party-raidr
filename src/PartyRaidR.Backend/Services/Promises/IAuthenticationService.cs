179b14c47f3b640540bbabaa13f12636d31f9e3f
Tóth Máté <t0thm4t3@gmail.com>
2025-11-28 12:58:53 +0100

    Authentication schema

---
+++ src/PartyRaidR.Backend/Services/Promises/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using PartyRaidR.Shared.Dtos;
+using PartyRaidR.Shared.Models.Responses;
+
+namespace PartyRaidR.Backend.Services.Promises
+{
+    public interface IAuthenticationService
+    {
+        Task<ServiceResponse<UserDto>> RegisterAsync(UserRegistrationDto user);
+        Task<ServiceResponse<string>> LoginAsync(UserLoginDto user);
+    }
+}
