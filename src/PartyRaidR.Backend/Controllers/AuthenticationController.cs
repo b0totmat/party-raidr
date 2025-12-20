ff4db93eae8d626901886c890a3d8a837afdd90c
Tóth Máté <t0thm4t3@gmail.com>
2025-12-20 15:59:02 +0100

    API endpoint for registration

---
+++ src/PartyRaidR.Backend/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyRaidR.Backend.Services;
+using PartyRaidR.Backend.Services.Promises;
+using PartyRaidR.Shared.Dtos.AuthenticationRequests;
+
+namespace PartyRaidR.Backend.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _service;
+
+        public AuthenticationController(AuthenticationService authService)
+        {
+            _service = authService;
+        }
+
+        [HttpGet("register")]
+        public async Task<object> RegisterAsync(UserRegistrationDto newUser)
+        {
+            var result = await _service.RegisterAsync(newUser);
+
+            if (result.Success)
+                return Ok(result);
+
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
