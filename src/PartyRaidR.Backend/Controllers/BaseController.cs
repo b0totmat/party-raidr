2e9f6e253fd9d9a08e72bb219b80cdc51f22bcd0
Tóth Máté <t0thm4t3@gmail.com>
2026-01-06 00:14:11 +0100

    Base API controller

---
+++ src/PartyRaidR.Backend/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyRaidR.Backend.Services.Base;
+using PartyRaidR.Shared.Models;
+
+namespace PartyRaidR.Backend.Controllers
+{
+    public abstract class BaseController<TModel, TDto> : ControllerBase
+        where TModel : class, IDbEntity<TModel>, new()
+        where TDto : class
+    {
+        protected readonly IBaseService<TModel, TDto> _service;
+
+        protected BaseController(IBaseService<TModel, TDto> service)
+        {
+            _service = service;
+        }
+    }
+}
