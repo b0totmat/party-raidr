beed16f2d2f040888118f58800753488f0fbdc49
Tóth Máté <t0thm4t3@gmail.com>
2026-01-16 15:37:34 +0100

    Navbar - Menu with Bootstrap

--- src/PartyRaidR.Web/src/components/Header.vue
+++ src/PartyRaidR.Web/src/components/Header.vue
@@ -1,6 +1,25 @@
-<script setup></script>
+<script setup>
+  import { RouterLink } from 'vue-router'
+</script>
 <template>
   <header>
-    <h1>Party RaidR</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Party RaidR</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/link">Link</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 </template>
